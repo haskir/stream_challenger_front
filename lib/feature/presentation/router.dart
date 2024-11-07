cbaaf009da63d61b3688982c1a7d21a99e4bfeda
unknown <haskird2@gmail.com>
2024-11-07 19:50:43 +0300

    routes inside body

---
+++ lib/feature/presentation/router.dart
@@ -0,0 +1,29 @@
+import 'package:go_router/go_router.dart';
+import 'package:stream_challenge/feature/presentation/widgets/body_widgets/challenge_create.dart';
+import 'package:stream_challenge/feature/presentation/widgets/body_widgets/challenges_list.dart';
+import 'package:stream_challenge/feature/presentation/widgets/body_widgets/profile.dart';
+import 'package:stream_challenge/feature/presentation/widgets/appbar_widgets/logo.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  routes: routes,
+);
+
+final List<GoRoute> routes = [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const LogoWidget(),
+  ),
+  GoRoute(
+    path: '/challenge_create',
+    builder: (context, state) => const ChallengeCreateWidget(),
+  ),
+  GoRoute(
+    path: '/challenges',
+    builder: (context, state) => const ChallengeListWidget(),
+  ),
+  GoRoute(
+    path: '/profile',
+    builder: (context, state) => const ProfileWidget(),
+  ),
+];
