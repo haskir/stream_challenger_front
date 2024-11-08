2488cced4a67f8fc66291731b7ac580d51a948fd
unknown <haskird2@gmail.com>
2024-11-08 18:01:53 +0300

    храню стринг токен в памяти

--- lib/core/auth_client.dart
+++ lib/core/auth_client.dart
@@ -12,11 +12,12 @@ abstract class AuthClient {
   Future<void> auth(BuildContext context);
   Future<void> logout();
   Future<AuthState> getState();
-  Future<AuthToken?> getUserInfo();
+  AuthToken? getUserInfo();
 }
 
 class AuthServiceHTML implements AuthClient {
   final Uri authUrl = Uri.parse('http://localhost:80/api/auth');
+  String? _token;
   final TokenRepo _tokenRepo = TokenRepo();
 
   // Создаем ValueNotifier для отслеживания состояния авторизации
@@ -34,11 +35,11 @@ class AuthServiceHTML implements AuthClient {
           String token = event.data;
           print("token: `$token`");
         }
+        _token = event.data;
         await _tokenRepo.setToken(event.data);
         newWindow.close();
 
-        // Обновляем состояние авторизации после получения токена
-        AuthToken? user = await getUserInfo();
+        AuthToken? user = getUserInfo();
         authStateNotifier.value = user == null
             ? AuthState(status: AuthStatus.unauthenticated)
             : AuthState(status: AuthStatus.authenticated, user: user);
@@ -47,18 +48,14 @@ class AuthServiceHTML implements AuthClient {
   }
 
   @override
-  Future<AuthToken?> getUserInfo() async {
-    String? token = await _tokenRepo.getToken();
-    if (token == null) {
-      return null;
-    }
-    Map<String, dynamic> decodedToken = JwtDecoder.decode(token);
-    return AuthToken.fromJson(decodedToken);
+  AuthToken? getUserInfo() {
+    if (_token == null) return null;
+    return AuthToken.fromJson(JwtDecoder.decode(_token!));
   }
 
   @override
   Future<AuthState> getState() async {
-    AuthToken? token = await getUserInfo();
+    AuthToken? token = getUserInfo();
     return token == null
         ? AuthState(status: AuthStatus.unauthenticated)
         : AuthState(status: AuthStatus.authenticated, user: token);
