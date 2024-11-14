e8736e5808992a4f709e60ffeccf7e2494314c08
haskir <haskird2@gmail.com>
2024-11-14 12:12:37 +0300

    Добавил пасхалку

--- lib/main_widgets/appbar_widgets/balance_widget.dart
+++ lib/main_widgets/appbar_widgets/balance_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stream_challenge/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 final Map _currency = {
   "RUB": "₽",
@@ -16,6 +17,9 @@ class BalanceWidget extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final balance = ref.watch(authStateProvider).user?.account;
+    final player =
+        AudioPlayer(); // Создаем аудиоплеер для воспроизведения звука
+
     if (balance == null) {
       return Center();
     }
@@ -25,6 +29,12 @@ class BalanceWidget extends ConsumerWidget {
         SizedBox(width: 3),
         TextButton(
           onPressed: () {},
+          onLongPress: () async {
+            await player.play(
+              AssetSource('sounds/green_is_good.mpeg'),
+              volume: 0.3,
+            );
+          },
           style: ElevatedButton.styleFrom(maximumSize: Size(65, 65)),
           child: Icon(Icons.add),
         ),
