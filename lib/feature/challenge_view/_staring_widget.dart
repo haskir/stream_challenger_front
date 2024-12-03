27aa16dbb0fae02b98f377c53817821221a939f2
unknown <haskird2@gmail.com>
2024-12-03 19:02:25 +0300

    star hovering

--- lib/feature/challenge_view/_staring_widget.dart
+++ lib/feature/challenge_view/_staring_widget.dart
@@ -18,6 +18,7 @@ class StarRating extends StatefulWidget {
 
 class StarRatingState extends State<StarRating> {
   late int _currentRating;
+  int _hoveredRating = 0; // Рейтинг при наведении мыши
 
   @override
   void initState() {
@@ -32,18 +33,31 @@ class StarRatingState extends State<StarRating> {
     widget.onRatingChanged(_currentRating);
   }
 
+  void _updateHoveredRating(int newHoveredRating) {
+    setState(() {
+      _hoveredRating = newHoveredRating;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: List.generate(widget.maxRating, (index) {
         final starIndex = index + 1;
-        return GestureDetector(
-          onTap: () => _updateRating(starIndex),
-          child: Icon(
-              starIndex <= _currentRating ? Icons.star : Icons.star_border,
-              color: starIndex <= _currentRating ? Colors.amber : Colors.grey,
-              size: 40),
+        final isHighlighted =
+            starIndex <= (_hoveredRating > 0 ? _hoveredRating : _currentRating);
+        return MouseRegion(
+          onEnter: (_) => _updateHoveredRating(starIndex),
+          onExit: (_) => _updateHoveredRating(0),
+          child: GestureDetector(
+            onTap: () => _updateRating(starIndex),
+            child: Icon(
+              isHighlighted ? Icons.star : Icons.star_border,
+              color: isHighlighted ? Colors.amber : Colors.grey,
+              size: 40,
+            ),
+          ),
         );
       }),
     );
